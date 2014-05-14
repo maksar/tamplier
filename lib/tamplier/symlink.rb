eacc0a053e44d63c3479dc2dda22b8a371d49c00
Alexander Shestakov <Maksar.mail@gmail.com>
2014-05-14 13:36:34 +0300

    Fixing defect with incorrect symlinks.

--- lib/tamplier/symlink.rb
+++ lib/tamplier/symlink.rb
@@ -2,8 +2,8 @@ module Tamplier
   class Symlink
     def call(root)
       ProblemIterator.new.call(root) do |sample_file, config_file|
-        FileUtils.ln_s(sample_file, config_file)
-        puts "Made a symlink from #{sample_file} pointing to #{config_file}."
+        FileUtils.symlink(sample_file.basename, config_file)
+        puts "Made a symlink from #{config_file} pointing to #{sample_file.basename}."
       end
     end
   end
