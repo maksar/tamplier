1a9b79788e52d12511aa1ae77d18299eb14c1dd3
Alexander Shestakov <Maksar.mail@gmail.com>
2014-05-17 12:27:50 +0300

    Fixing error with environment loading.

--- lib/tamplier.rb
+++ lib/tamplier.rb
@@ -4,6 +4,7 @@ require 'active_support/all'
 
 require_relative 'tamplier/version'
 require_relative 'tamplier/exceptions/configuration_exception'
+require_relative 'tamplier/iterator'
 require_relative 'tamplier/validator'
 
 require 'tamplier/railtie' if defined?(Rails)
